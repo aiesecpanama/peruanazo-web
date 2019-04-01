7825f1973a76fe004e90b6e148841e25e788315a
Danillo Santos <danillosantosdejesus@gmail.com>
2019-04-01 11:37:17 -0300

    feat(i18n): added missing translations

--- src/app/form-gv/form-gv.component.ts
+++ src/app/form-gv/form-gv.component.ts
@@ -67,8 +67,8 @@ export class FormGvComponent implements OnInit {
 
   placeholderBirthdate: string;
 
-  personalData: boolean = false;
-  studyData: boolean = true;
+  personalData: boolean = true;
+  studyData: boolean = false;
 
   invalidEmail: boolean = false;
   invalidPassword: boolean = false;
