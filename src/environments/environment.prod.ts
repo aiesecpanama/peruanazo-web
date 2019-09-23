97daeec45767448e1bd3f94dec92ba44ea9ac619
Paola Latino <paolalat188@gmail.com>
2019-09-23 14:13:01 -0500

    correccion links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://api.aiesec.pe'
+  apiUrl : 'https://api.aiesec.org.pa'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
   // apiUrl : 'https://qa-databazi.aiesec.org.br'
   //apiUrl : 'http://3.210.8.113'
   //apiUrl : 'http://databazi-production-lb-480157463.us-east-1.elb.amazonaws.com'
-  apiUrl : 'https://api.aiesec.pe'
+  apiUrl : 'https://api.aiesec.org.pa'
 };
